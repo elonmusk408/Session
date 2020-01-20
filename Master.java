34c17b572fe70e88cb3096db98b91a30378e3adf
elonmusk408 <naresh721b@gmail.com>
2020-01-20 01:26:55 +0000

    Revert "added html code" not required currently for this module
    This reverts commit 5894722b22c133d3c1bbe3364972794fc3672c9a.

--- Master.java
+++ Master.java
@@ -1,22 +1,3 @@
-helloeiharset="utf-8">
-  <title>Sample Deployment</title>
-  <style>
-    body {
-      color: #ffffff;
-      background-color: #007f3f;
-      font-family: Arial, sans-serif;
-      font-size: 14px;
-    }
-    
-    h1 {
-      font-size: 500%;
-      font-weight: normal;
-      margin-bottom: 0;
-    }
-    
-    h2 {
-      font-size: 200%;
-      font-weight: normal;
-      margin-bottom: 0;
-ndfwr
+hello
+e
 wferdv

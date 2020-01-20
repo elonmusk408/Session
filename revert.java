15a587007656bb5777bf39dc85e830014c84c845
elonmusk408 <naresh721b@gmail.com>
2020-01-20 01:29:26 +0000

    reverting exampe

---
+++ revert.java
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Sample Deployment</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: #007f3f;
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {

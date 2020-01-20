5894722b22c133d3c1bbe3364972794fc3672c9a
elonmusk408 <naresh721b@gmail.com>
2020-01-20 01:25:17 +0000

    added html code

--- Master.java
+++ Master.java
@@ -1,3 +1,22 @@
-hello
-e
+helloeiharset="utf-8">
+  <title>Sample Deployment</title>
+  <style>
+    body {
+      color: #ffffff;
+      background-color: #007f3f;
+      font-family: Arial, sans-serif;
+      font-size: 14px;
+    }
+    
+    h1 {
+      font-size: 500%;
+      font-weight: normal;
+      margin-bottom: 0;
+    }
+    
+    h2 {
+      font-size: 200%;
+      font-weight: normal;
+      margin-bottom: 0;
+ndfwr
 wferdv
